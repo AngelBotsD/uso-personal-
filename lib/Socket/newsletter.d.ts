7f80f8f427e80e97d7a996ed19b73fe07106b3b7
Ado <meadonix@gmail.com>
2026-01-03 21:03:02 -0600

    Actualizar newsletter.d.ts

--- lib/Socket/newsletter.d.ts
+++ lib/Socket/newsletter.d.ts
@@ -60,6 +60,18 @@ export declare const makeNewsletterSocket: (config: SocketConfig) => {
     processingMutex: {
         mutex<T>(code: () => Promise<T> | T): Promise<T>;
     };
+    messageMutex: {
+        mutex<T>(code: () => Promise<T> | T): Promise<T>;
+    };
+    receiptMutex: {
+        mutex<T>(code: () => Promise<T> | T): Promise<T>;
+    };
+    notificationMutex: {
+        mutex<T>(code: () => Promise<T> | T): Promise<T>;
+    };
+    appStatePatchMutex: {
+        mutex<T>(code: () => Promise<T> | T): Promise<T>;
+    };
     fetchPrivacySettings: (force?: boolean) => Promise<{
         [_: string]: string;
     }>;
