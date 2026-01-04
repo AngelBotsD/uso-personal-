1e541bb44f8284682b61fc5432298f80e0e2e557
Ado <meadonix@gmail.com>
2026-01-03 21:01:15 -0600

    Actualizar chats.d.ts

--- lib/Socket/chats.d.ts
+++ lib/Socket/chats.d.ts
@@ -7,6 +7,18 @@ export declare const makeChatsSocket: (config: SocketConfig) => {
     processingMutex: {
         mutex<T>(code: () => Promise<T> | T): Promise<T>;
     };
+    messageMutex: {
+        mutex<T>(code: () => Promise<T> | T): Promise<T>;
+    };
+    receiptMutex: {
+        mutex<T>(code: () => Promise<T> | T): Promise<T>;
+    };
+    notificationMutex: {
+        mutex<T>(code: () => Promise<T> | T): Promise<T>;
+    };
+    appStatePatchMutex: {
+        mutex<T>(code: () => Promise<T> | T): Promise<T>;
+    };
     fetchPrivacySettings: (force?: boolean) => Promise<{
         [_: string]: string;
     }>;
